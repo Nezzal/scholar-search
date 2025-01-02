a0c225de4c60fb53f7ab490b8af40774d5f22f16
Nezzal Abdelmalek <48630318+Nezzal@users.noreply.github.com>
2025-01-02 17:15:34 +0100

    Create next.config.js
    création du fichier next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',  // Pour générer une version statique
+  basePath: '/scholar-search',  // Pour le bon fonctionnement sur GitHub Pages
+  images: {
+    unoptimized: true,  // Nécessaire pour le déploiement statique
+  },
+}
+
+module.exports = nextConfig
